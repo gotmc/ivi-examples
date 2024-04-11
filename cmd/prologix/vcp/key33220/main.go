b8989fa2722ab7e99ef2a844e7d45d8f19d2bb5c
Matthew Rankin <matthew@cumulusware.com>
2024-04-11 12:06:07 -0500

    Use port flog for vcp/key33220 example

--- cmd/prologix/vcp/key33220/main.go
+++ cmd/prologix/vcp/key33220/main.go
@@ -6,6 +6,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/gotmc/ivi/fgen"
@@ -14,8 +15,26 @@ import (
 	"github.com/gotmc/prologix/driver/vcp"
 )
 
+var (
+	serialPort string
+)
+
+func init() {
+	// Get Virtual COM Port (VCP) serial port for Prologix.
+	flag.StringVar(
+		&serialPort,
+		"port",
+		"/dev/tty.usbserial-PX8X3YR6",
+		"Serial port for Prologix VCP GPIB controller",
+	)
+}
+
 func main() {
-	serialPort := "/dev/tty.usbserial-PX484GRU"
+	log.Println("IVI Prologix VCP GPIB Keysight 33220A Example Application")
+	// Parse the flags
+	flag.Parse()
+
+	log.Printf("Serial port = %s", serialPort)
 	vcp, err := vcp.NewVCP(serialPort)
 	if err != nil {
 		log.Fatal(err)
@@ -39,38 +58,6 @@ func main() {
 		log.Fatalf("IVI instrument error: %s", err)
 	}
 
-	// Channel specific methods can be accessed directly from the instrument
-	// using 0-based index to select the desirec channel.
-	fg.Channels[0].DisableOutput()
-	fg.Channels[0].SetAmplitude(0.4)
-
-	// Alternatively, the channel can be assigned to a variable.
-	ch := fg.Channels[0]
-	ch.SetStandardWaveform(fgen.Sine)
-	ch.SetDCOffset(0.1)
-	ch.SetFrequency(2340)
-
-	// Instead of configuring attributes of a standard waveform individually, the
-	// standard waveform can be configured using a single method.
-	ch.ConfigureStandardWaveform(fgen.RampUp, 0.4, 0.1, 2340, 0)
-	ch.EnableOutput()
-
-	// Query the fg
-	freq, err := ch.Frequency()
-	if err != nil {
-		log.Printf("error querying frequency: %s", err)
-	}
-	log.Printf("Frequency = %.0f Hz", freq)
-	amp, err := ch.Amplitude()
-	if err != nil {
-		log.Printf("error querying amplitude: %s", err)
-	}
-	log.Printf("Amplitude = %.3f Vpp", amp)
-	wave, err := ch.StandardWaveform()
-	if err != nil {
-		log.Printf("error querying standard waveform: %s", err)
-	}
-	log.Printf("Standard waveform = %s", wave)
 	mfr, err := fg.InstrumentManufacturer()
 	if err != nil {
 		log.Printf("error querying instrument manufacturer: %s", err)
@@ -91,6 +78,52 @@ func main() {
 		log.Printf("error querying firmware revision: %s", err)
 	}
 	log.Printf("Firmware revision = %s", fw)
+	// Channel specific methods can be accessed directly from the instrument
+	// using 0-based index to select the desirec channel.
+	if err = fg.Channels[0].DisableOutput(); err != nil {
+		log.Fatalf("error disabling output on ch0: %s", err)
+	}
+	if err = fg.Channels[0].SetAmplitude(0.4); err != nil {
+		log.Fatalf("error setting the amplitude on ch0: %s", err)
+	}
+
+	// Alternatively, the channel can be assigned to a variable.
+	ch := fg.Channels[0]
+	if err = ch.SetStandardWaveform(fgen.Sine); err != nil {
+		log.Fatalf("error setting the standard waveform: %s", err)
+	}
+	if err = ch.SetDCOffset(0.3); err != nil {
+		log.Fatalf("error setting DC offest: %s", err)
+	}
+	if err = ch.SetFrequency(2840); err != nil {
+		log.Fatalf("error setting frequency: %s", err)
+	}
+
+	// Instead of configuring attributes of a standard waveform individually, the
+	// standard waveform can be configured using a single method.
+	if err = ch.ConfigureStandardWaveform(fgen.Square, 0.7, 0.2, 2710, 0); err != nil {
+		log.Fatalf("error configuring standard waveform: %s", err)
+	}
+	if err = ch.EnableOutput(); err != nil {
+		log.Fatalf("error enabling output: %s", err)
+	}
+
+	// Query the fg
+	freq, err := ch.Frequency()
+	if err != nil {
+		log.Printf("error querying frequency: %s", err)
+	}
+	log.Printf("Frequency = %.0f Hz", freq)
+	amp, err := ch.Amplitude()
+	if err != nil {
+		log.Printf("error querying amplitude: %s", err)
+	}
+	log.Printf("Amplitude = %.3f Vpp", amp)
+	wave, err := ch.StandardWaveform()
+	if err != nil {
+		log.Printf("error querying standard waveform: %s", err)
+	}
+	log.Printf("Standard waveform = %s", wave)
 
 	// Return local control to the front panel.
 	err = gpib.FrontPanel(true)
